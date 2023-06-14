98fc86296b38220f8229e3106509576a97999e24
WebDevAlok <bhushan.alok13@gmail.com>
2023-06-14 17:00:34 +0530

    Initial Commit

---
+++ src/test/java/com/example/springfreemarkerexample/SpringFreemarkerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfreemarkerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFreemarkerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
